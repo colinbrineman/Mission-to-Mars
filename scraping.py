2b5121dc08079271a48aa8e9d48bf391b780533a
Colin Brineman <colinbrineman@Colins-MacBook.local>
2022-02-14 03:52:51 -0500

    challenge

--- scraping.py
+++ scraping.py
@@ -7,13 +7,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 def scrape_all():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
-    news_title, news_paragraph = mars_news(browser)
+    news_title, news_p = mars_news(browser)
+    hemispheres = mars_hemispheres(browser)
     data = {
         "news_title": news_title,
-        "news_paragraph": news_paragraph,
+        "news_paragraph": news_p,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        'hemispheres': hemispheres
     }
     browser.quit()
     return data
@@ -48,12 +50,44 @@ def featured_image(browser):
 
 def mars_facts():
     try:
-        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
+        df = pd.read_html(
+            'https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
     except BaseException:
         return None
-    df.columns=['Description', 'Mars', 'Earth']
+    df.columns = ['Description', 'Mars', 'Earth']
     df.set_index('Description', inplace=True)
     return df.to_html(classes="table table-striped")
+
+def mars_hemispheres(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    html = browser.html
+    hemisphere_soup = soup(html, 'html.parser')
+    items = hemisphere_soup.find_all('div', class_="item")
+    hemisphere_urls = []
+    try:
+        for item in items:
+            hemisphere_rel = item.a['href']
+            hemisphere_url = f'https://marshemispheres.com/{hemisphere_rel}'
+            hemisphere_urls.append(hemisphere_url)
+    except AttributeError:
+        return None
+    img_urls = []
+    titles = []
+    try:
+        for hemisphere_url in hemisphere_urls:
+            browser.visit(hemisphere_url)
+            html = browser.html
+            imgs_soup = soup(html, 'html.parser')
+            img_rel = imgs_soup.find('img', class_='wide-image').get('src')
+            img_url = f'https://marshemispheres.com/{img_rel}'
+            img_urls.append(img_url)
+            title = imgs_soup.find('h2', class_='title').get_text()
+            titles.append(title)
+    except AttributeError:
+        return None
+    hemispheres = [{'image': i, 'caption': t} for i, t in zip(img_urls, titles)]
+    return hemispheres
 
 if __name__ == "__main__":
     print(scrape_all())
